5ed3528d0340fb54c86258bc3101908ec6cceff6
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2023-03-26 02:37:50 +0530

    add: wallet login using ethers
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

---
+++ chainfolio-app/components/Wallet/index.tsx
@@ -0,0 +1,38 @@
+import { BrowserProvider, ethers, getDefaultProvider, parseUnits, Signer } from "ethers";
+declare var window: any
+
+
+interface IWalletProps {
+    setWalletAddress:any,
+    setSigner: any,
+    setIsWalletConnected: any
+}
+
+export const WalletConnect: React.FC<IWalletProps> = ({setIsWalletConnected, setSigner, setWalletAddress }) => {
+
+
+    async function handleWalletConnect() {
+
+        if (window.ethereum) {
+            let _provider = null;
+            let _signer = null;
+            _provider = new BrowserProvider(window.ethereum)
+            _signer = await _provider.getSigner();
+            let _address = await _signer.getAddress();
+            _address = ethers.getAddress(_address)
+            setWalletAddress(_address)
+            setSigner(_signer)
+            setIsWalletConnected(true)
+
+        } else {
+            alert('Install Metamask');
+        }
+    }
+    return (
+        <>
+            Hello World
+
+            <button onClick={handleWalletConnect}> connect to metamask</button>
+        </>
+    )
+}
